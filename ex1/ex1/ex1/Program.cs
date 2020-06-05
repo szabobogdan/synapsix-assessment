a8829803cccd8246120c895b7864a535ee400baa
Szabo Bogdan <szabobogdan89@yahoo.com>
2020-06-05 12:36:30 +0300

    First commit

---
+++ ex1/ex1/ex1/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ex1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 0, j;
+            string line;
+            string markers;
+            Console.WriteLine("Choose the markers: ");
+            markers = Console.ReadLine();// it should not contain spaces between markers to work properly
+            
+           
+                      
+            System.IO.StreamReader file=new System.IO.StreamReader("intrare.txt");//the input string is read from a file
+            Console.WriteLine("Input string: ");
+            while ((line = file.ReadLine()) != null)
+            {
+                Console.WriteLine(line);
+            }
+
+            Console.WriteLine("\n");
+
+            file.DiscardBufferedData();
+            file.BaseStream.Seek(0, System.IO.SeekOrigin.Begin);
+            Console.WriteLine("The string after stripping the text that succedes the markers : ");
+            while ((line = file.ReadLine()) != null)// start reading again from the file, from the first line
+            {
+
+                for (j = 0; j < markers.Length; j++)
+                {
+                    i = line.IndexOf(markers[j], 0);
+                    if (i != -1)// IndexOf returns -1 in case it doesn't find the character in the string
+                    {
+                        break;
+
+                    }
+                }
+                if (i != -1)
+                {
+                    Console.WriteLine(line.Remove(i));
+
+                }
+                else
+                {
+                    Console.WriteLine(line);
+                }
+
+
+            }
+               file.Close();
+                       
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ex2/ex2/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text;
+using System.IO;
+namespace ex2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int ok = 1;
+          
+            String input = File.ReadAllText("input.txt");
+
+            int i = 0, j = 0;
+            int[,] arr = new int[100, 100];
+            //read the structure from a file
+            foreach (var row in input.Split('\n'))
+            {
+                j = 0;
+                foreach (var col in row.Trim().Split(' '))
+                {
+                    arr[i, j] = int.Parse(col.Trim());
+                    j++;
+                }
+                i++;
+            }
+            //validating all the conditions
+            if(i==j)
+            {
+                double sr;
+                sr = Math.Sqrt(i);
+                
+                if((sr-Math.Floor(sr)==0))
+                {
+                    int n = i;
+                    for(i=0;i<n;i++)
+                    {
+                        for(j=0;j<n;j++)
+                        {
+
+                            if(arr[i,j]<=0||arr[i,j]>9)
+                            {
+                                ok = 0;
+                                break;
+                            }
+                         
+                        }
+                    }
+                    
+                }
+                else
+                {
+                    ok = 0;
+                }
+            }
+            else
+            {
+                ok = 0;
+            }
+            if(ok==0)
+            {
+                Console.WriteLine("Not a valid Sudoku data structure.");
+            }
+            else
+            {
+                Console.WriteLine("Valid Sudoku data structure.");
+            }
+            Console.ReadLine();
+        }
+    }
+}
